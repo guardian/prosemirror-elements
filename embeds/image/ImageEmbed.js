642145757f1743bda9912660442c2fb198eb750a
RichieAHB <richard.beddington@guardian.co.uk>
2018-07-25 17:35:36 +0100

    Adds some config to the image embed and demos how easy it is to change

--- embeds/image/ImageEmbed.js
+++ embeds/image/ImageEmbed.js
@@ -1,11 +1,39 @@
 import { h } from 'preact';
 
-const ImageEmbed = ({ fields: { name }, updateFields }) => (
-  <input
-    type="text"
-    value={name}
-    onInput={e => updateFields({ name: e.target.value })}
-  />
+const ImageEmbed = ({
+  fields: { caption, src, alt },
+  updateFields,
+  editSrc
+}) => (
+  <div>
+    <img style={{ width: '250px', height: 'auto' }} src={src} alt={alt} />
+    <label>
+      Caption
+      <input
+        type="text"
+        value={caption}
+        onInput={e => updateFields({ caption: e.target.value })}
+      />
+    </label>
+    <label>
+      Alt
+      <input
+        type="text"
+        value={alt}
+        onInput={e => updateFields({ alt: e.target.value })}
+      />
+    </label>
+    {editSrc && (
+      <label>
+        Src
+        <input
+          type="text"
+          value={src}
+          onInput={e => updateFields({ src: e.target.value })}
+        />
+      </label>
+    )}
+  </div>
 );
 
 export default ImageEmbed;
