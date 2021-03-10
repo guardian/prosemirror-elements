e662e2502ee19720c79c93eb220fa0552d34bd3d
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-03-10 18:35:15 +0000

    Add webpack preprocessor, and wire to Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,10 +12,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+
+module.exports = (on) => {
+  on('file:preprocessor', webpackPreprocessor())
 }
