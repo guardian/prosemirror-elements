8c6095efe45dfa214dabe332a4d2487e39efc859
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-05-12 18:41:00 +0100

    Remove fixed schema from image embed

--- src/embeds/image/embed.tsx
+++ src/embeds/image/embed.tsx
@@ -1,38 +1,6 @@
-import type { NodeSpec } from "prosemirror-model";
 import React from "react";
 import { createReactEmbedRenderer } from "../../mounters/react/mount";
 import { ImageEmbed } from "./ImageEmbed";
-
-export const imageSchemaSpec: NodeSpec = {
-  caption: {
-    group: "block",
-    content: "paragraph",
-    toDOM() {
-      return ["div", { class: "imageNative-caption" }, 0];
-    },
-    parseDOM: [{ tag: "div" }],
-  },
-  altText: {
-    group: "block",
-    content: "paragraph",
-    toDOM() {
-      return ["div", { class: "imageNative-altText" }, 0];
-    },
-    parseDOM: [{ tag: "div" }],
-  },
-  imageEmbed: {
-    group: "caption altText",
-    attrs: {
-      type: {},
-      fields: {
-        default: {},
-      },
-      hasErrors: {
-        default: false,
-      },
-    },
-  },
-};
 
 export const imageProps = [
   {
