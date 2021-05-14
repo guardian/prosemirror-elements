2fc468998a8266b5693273c6ca578578c48238fd
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-05-14 08:32:25 +0100

    Add polymorphic PropField component, and add more data to the props passed to components to allow it to choose which field to render

---
+++ src/mounters/react/propFields/PropField.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import type { NodeViewProp } from "../../../types/Embed";
+import { RichTextPropField } from "./RichTextPropField";
+
+type Props = {
+  nodeViewProp: NodeViewProp;
+};
+
+export const getPropFieldTestId = (name: string) => `PropField-${name}`;
+
+export const PropField: React.FunctionComponent<Props> = ({ nodeViewProp }) => {
+  switch (nodeViewProp.prop.type) {
+    case "richText":
+      return <RichTextPropField nodeViewProp={nodeViewProp} />;
+  }
+};
