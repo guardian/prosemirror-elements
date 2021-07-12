c1dcba4d4c0ff72fd87e4735fa67dd3d5a2476c3
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-07-12 11:43:10 +0100

    Improve test naming

--- cypress/tests/Editor.spec.ts
+++ cypress/tests/Editor.spec.ts
@@ -1,18 +1,16 @@
 import { typeIntoProsemirror } from "../helpers/editor";
 
-describe("ImageElement", () => {
+describe("Core editor behaviour", () => {
   beforeEach(() => cy.visit("/"));
 
-  describe("Fields", () => {
-    it("should accept editor input", () => {
-      typeIntoProsemirror("{selectall}Text");
-      cy.get(".ProseMirror > p").should("have.text", "Text");
-    });
+  it("should accept editor input", () => {
+    typeIntoProsemirror("{selectall}Text");
+    cy.get(".ProseMirror > p").should("have.text", "Text");
+  });
 
-    it("should persist user input in localstorage", () => {
-      typeIntoProsemirror("{selectall}Text");
-      cy.reload();
-      cy.get(".ProseMirror > p").should("have.text", "Text");
-    });
+  it("should persist user input in localstorage", () => {
+    typeIntoProsemirror("{selectall}Text");
+    cy.reload();
+    cy.get(".ProseMirror > p").should("have.text", "Text");
   });
 });
