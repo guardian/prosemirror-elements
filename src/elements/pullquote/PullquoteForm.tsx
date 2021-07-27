1b971e74a9c6026d19a306b8be448cf3716050dc
demetrios_skamiotis <demetrios.skamiotis@guardian.co.uk>
2021-07-27 11:41:52 +0100

    Adds style layout to fields and custom-dropdown

--- src/elements/pullquote/PullquoteForm.tsx
+++ src/elements/pullquote/PullquoteForm.tsx
@@ -1,8 +1,16 @@
+import { css } from "@emotion/react";
+import { space } from "@guardian/src-foundations";
 import React from "react";
+import { CustomDropdown } from "../../editorial-source-components/CustomDropdown";
 import { Label } from "../../editorial-source-components/Label";
+import type { Option } from "../../plugin/fieldViews/DropdownFieldView";
 import type { FieldNameToValueMap } from "../../plugin/fieldViews/helpers";
-import type { FieldNameToFieldViewSpec } from "../../plugin/types/Element";
-import { FieldView } from "../../renderers/react/FieldView";
+import type {
+  CustomFieldViewSpec,
+  FieldNameToFieldViewSpec,
+} from "../../plugin/types/Element";
+import { FieldView, getFieldViewTestId } from "../../renderers/react/FieldView";
+import { useCustomFieldViewState } from "../../renderers/react/useCustomFieldViewState";
 import type { createPullquoteFields } from "./PullquoteSpec";
 
 type Props = {
@@ -13,6 +21,21 @@ type Props = {
   >;
 };
 
+const wrapper = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const column = css`
+  width: 33%;
+`;
+
+const primaryColumn = css`
+  width: 67%;
+  padding-right: ${space[3]}px;
+`;
+
 export const PullquoteElementTestId = "PullquoteElement";
 
 export const PullquoteElementForm: React.FunctionComponent<Props> = ({
@@ -20,12 +43,41 @@ export const PullquoteElementForm: React.FunctionComponent<Props> = ({
   fieldViewSpecMap: fieldViewSpecs,
 }) => (
   <div data-cy={PullquoteElementTestId}>
-    <FieldView fieldViewSpec={fieldViewSpecs.pullquote} />
-    <FieldView fieldViewSpec={fieldViewSpecs.attribution} />
-    <FieldView fieldViewSpec={fieldViewSpecs.weighting} />
+    <div css={wrapper}>
+      <div css={[column, primaryColumn]}>
+        <FieldView fieldViewSpec={fieldViewSpecs.pullquote} />
+      </div>
+      <div css={column}>
+        <FieldView fieldViewSpec={fieldViewSpecs.attribution} />
+        <CustomDropdownView fieldViewSpec={fieldViewSpecs.weighting} />
+      </div>
+    </div>
     <hr />
     <Label>Element errors</Label>
     <pre>{JSON.stringify(errors)}</pre>
     <hr />
   </div>
 );
+
+type CustomDropdownViewProps = {
+  fieldViewSpec: CustomFieldViewSpec<string, Array<Option<string>>>;
+};
+
+const CustomDropdownView = ({ fieldViewSpec }: CustomDropdownViewProps) => {
+  const [selectedElement, setSelectFieldsRef] = useCustomFieldViewState(
+    fieldViewSpec
+  );
+  return (
+    <CustomDropdown
+      options={fieldViewSpec.fieldSpec.props}
+      selected={selectedElement}
+      label={fieldViewSpec.name}
+      onChange={(event) => {
+        if (setSelectFieldsRef.current) {
+          setSelectFieldsRef.current(event.target.value);
+        }
+      }}
+      dataCy={getFieldViewTestId(fieldViewSpec.name)}
+    />
+  );
+};
