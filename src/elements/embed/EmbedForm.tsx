7747a37162796121b7e5527eeb1c8cc194efac2a
Rhys Mills <rhystmills@gmail.com>
2021-08-02 14:34:48 +0100

    adds embed element fields

---
+++ src/elements/embed/EmbedForm.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Label } from "../../editorial-source-components/Label";
+import type { FieldNameToValueMap } from "../../plugin/fieldViews/helpers";
+import type { FieldNameToFieldViewSpec } from "../../plugin/types/Element";
+import { FieldView } from "../../renderers/react/FieldView";
+import type { createEmbedFields } from "./EmbedSpec";
+
+type Props = {
+  fields: FieldNameToValueMap<ReturnType<typeof createEmbedFields>>;
+  errors: Record<string, string[]>;
+  fieldViewSpecMap: FieldNameToFieldViewSpec<
+    ReturnType<typeof createEmbedFields>
+  >;
+};
+
+export const EmbedElementTestId = "EmbedElement";
+
+export const EmbedElementForm: React.FunctionComponent<Props> = ({
+  fields,
+  errors,
+  fieldViewSpecMap: fieldViewSpecs,
+}) => (
+  <div data-cy={EmbedElementTestId}>
+    <FieldView fieldViewSpec={fieldViewSpecs.weighting} />
+    <FieldView fieldViewSpec={fieldViewSpecs.sourceUrl} />
+    <FieldView fieldViewSpec={fieldViewSpecs.embedCode} />
+    <FieldView fieldViewSpec={fieldViewSpecs.caption} />
+    <FieldView fieldViewSpec={fieldViewSpecs.altText} />
+    <FieldView fieldViewSpec={fieldViewSpecs.required} />
+
+    <hr />
+    <Label>Element errors</Label>
+    <pre>{JSON.stringify(errors)}</pre>
+    <hr />
+    <Label>Element values</Label>
+    <pre>{JSON.stringify(fields)}</pre>
+  </div>
+);
