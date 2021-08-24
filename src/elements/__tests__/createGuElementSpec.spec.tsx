5fb836eedfebab74137d34bb6d31c245925630a8
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-08-24 12:15:41 +0100

    feat: Add gu element creator (#85)
    * Add gu element creator
    
    * Amend transformer function and add test

---
+++ src/elements/__tests__/createGuElementSpec.spec.tsx
@@ -0,0 +1,45 @@
+import type { Node } from "prosemirror-model";
+import { createTextField } from "../../plugin/fieldViews/TextFieldView";
+import { createEditorWithElements } from "../../plugin/helpers/test";
+import { createGuElementSpec } from "../createGuElementSpec";
+
+const guElement = createGuElementSpec(
+  { exampleField: createTextField() },
+  () => <p></p>,
+  () => null
+);
+
+describe("createGuElementSpec", () => {
+  it("should transform data with the provided transformer", () => {
+    const {
+      getElementDataFromNode,
+      serializer,
+      view,
+      insertElement,
+    } = createEditorWithElements({
+      guElement,
+    });
+
+    const elementData = {
+      assets: [],
+      fields: { exampleField: "", isMandatory: true },
+    };
+
+    // This is effectively a roundtrip – insert element transforms on the way in ...
+    insertElement({
+      elementName: "guElement",
+      values: elementData,
+    })(view.state, view.dispatch);
+
+    // ... and getElementDataFromNode transforms on the way out.
+    const elementDataFromNode = getElementDataFromNode(
+      view.state.doc.content.firstChild as Node,
+      serializer
+    );
+
+    expect(elementDataFromNode).toEqual({
+      elementName: "guElement",
+      values: elementData,
+    });
+  });
+});
---
+++ src/elements/createGuElementSpec.ts
@@ -0,0 +1,41 @@
+import type { ReactElement } from "react";
+import type { Validator } from "../plugin/elementSpec";
+import type { FieldNameToValueMap } from "../plugin/fieldViews/helpers";
+import type { Consumer } from "../plugin/types/Consumer";
+import type { FieldDescriptions } from "../plugin/types/Element";
+import { createReactElementSpec } from "../renderers/react/createReactElementSpec";
+
+type FlexibleModelElement<FDesc extends FieldDescriptions<string>> = {
+  fields: Omit<FieldNameToValueMap<FDesc>, "assets"> & { isMandatory: boolean };
+  assets: string[];
+};
+
+/**
+ * Creates an element that is rendered by React, and transforms its data
+ * into a format compatible with flexible-model. See the model at
+ * https://github.com/guardian/flexible-model/blob/main/src/main/thrift/content.thrift
+ */
+export const createGuElementSpec = <FDesc extends FieldDescriptions<string>>(
+  FieldDescriptions: FDesc,
+  consumer: Consumer<ReactElement, FDesc>,
+  validate: Validator<FDesc>,
+  isMandatory = true
+) => {
+  return createReactElementSpec(FieldDescriptions, consumer, validate, {
+    transformElementDataIn: ({
+      assets = [],
+      fields,
+    }: FlexibleModelElement<FDesc>) => {
+      return { ...fields, assets } as FieldNameToValueMap<FDesc>;
+    },
+    transformElementDataOut: ({
+      assets,
+      ...fields
+    }: FieldNameToValueMap<FDesc>) => {
+      return {
+        assets: assets || [],
+        fields: { ...fields, isMandatory },
+      } as FlexibleModelElement<FDesc>;
+    },
+  });
+};
