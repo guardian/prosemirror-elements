68190977f196140715e3f9053fd4993314322e30
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-09-09 10:11:51 +0100

    Allow error messages to wrap beneath labels when they exceed a single line

--- src/editorial-source-components/Label.tsx
+++ src/editorial-source-components/Label.tsx
@@ -6,6 +6,10 @@ export const labelStyles = css`
   ${textSans.small({ fontWeight: "bold", lineHeight: "loose" })}
   font-family: "Guardian Agate Sans";
   display: block;
+  // Ensure that the label pushes error messages to the rhs
+  // if they occupy the same line, to ensure the error message
+  // is right-aligned.
+  flex-grow: 1;
 `;
 
 export const Label = styled.label`
