f2ee5757314d0f0bf20af05638fb318244ab98a4
demetrios_skamiotis <demetrios.skamiotis@guardian.co.uk>
2021-09-09 14:29:01 +0100

    Add ButtonProps to component

--- src/editorial-source-components/Button.tsx
+++ src/editorial-source-components/Button.tsx
@@ -1,25 +1,13 @@
 import { css } from "@emotion/react";
+import type { ButtonProps } from "@guardian/src-button";
 import { Button as SourceButton } from "@guardian/src-button";
-import type { ReactElement } from "react";
 import React from "react";
 
 export const buttonStyles = css`
   font-family: "Guardian Agate Sans";
 `;
 
-type Size = "default" | "small" | "xsmall";
-type ButtonPriority = "primary" | "secondary" | "tertiary" | "subdued";
-type IconSide = "left" | "right";
-
-type Props = {
-  iconSide?: IconSide;
-  priority?: ButtonPriority;
-  size?: Size;
-  icon?: ReactElement;
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
-
-export const Button: React.FunctionComponent<Props> = ({
+export const Button: React.FunctionComponent<ButtonProps> = ({
   children,
   size,
   ...props
