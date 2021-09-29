a5758418522ce0f6b8ec7252e79f74655321851d
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-09-29 09:11:06 +0100

    Add placeholder plugin

---
+++ src/plugin/helpers/placeholder.ts
@@ -0,0 +1,53 @@
+import type { Node } from "prosemirror-model";
+import { Plugin } from "prosemirror-state";
+import { Decoration, DecorationSet } from "prosemirror-view";
+
+const placeholderAttribute = "data-cy-is-placeholder";
+
+const getPlaceholder = (text: string) => {
+  const span = document.createElement("span");
+  span.style.display = "inline-block";
+  span.style.height = "0px";
+  span.style.width = "0px";
+  span.style.whiteSpace = "nowrap";
+  span.style.fontStyle = "italic";
+  span.style.color = "#777";
+  span.style.pointerEvents = "none";
+  span.style.cursor = "text";
+  span.draggable = false;
+  span.innerHTML = text;
+  span.setAttribute(placeholderAttribute, "true");
+  return span;
+};
+
+export const containsPlaceholder = (element: HTMLElement): boolean =>
+  !!element.querySelector(`span[${placeholderAttribute}]`);
+
+/**
+ * Get the first placeholder position in the document – assumed
+ * to be the starting position of the deepest first child.
+ */
+const getFirstPlaceholderPosition = (node: Node, currentPos = 0): number =>
+  node.firstChild
+    ? getFirstPlaceholderPosition(
+        node.firstChild,
+        currentPos + node.content.size
+      )
+    : currentPos + node.content.size;
+
+export const createPlaceholderPlugin = (text: string) =>
+  new Plugin({
+    props: {
+      decorations: ({ doc }) => {
+        if (doc.textContent) {
+          return DecorationSet.empty;
+        }
+
+        // If the document contains inline content only, just place the widget at its start.
+        const pos = doc.inlineContent ? 0 : getFirstPlaceholderPosition(doc);
+        return DecorationSet.create(doc, [
+          Decoration.widget(pos, getPlaceholder(text)),
+        ]);
+      },
+    },
+  });
