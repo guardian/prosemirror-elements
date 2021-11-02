d9628d3cd1a9fef1102a577dacf7b191f0cce6c4
Rhys Mills <rhystmills@gmail.com>
2021-11-02 16:52:20 +0000

    Simplify iframe

--- src/elements/embed/embedComponents/Preview.tsx
+++ src/elements/embed/embedComponents/Preview.tsx
@@ -1,7 +1,4 @@
 import { css } from "@emotion/react";
-import type { ReactChild } from "react";
-import { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
 import { Label } from "../../../editorial-source-components/Label";
 import { unescapeHtml } from "./embedUtils";
 
@@ -10,36 +7,11 @@ const iframe = css`
   width: 100%;
 `;
 
-export const IFrame = ({ children }: { children: ReactChild }) => {
-  const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
-  const mountNode = contentRef?.contentWindow?.document.body;
-
-  // Set some default styles for the iframe body
-  if (mountNode) {
-    mountNode.setAttribute(
-      "style",
-      "font-family: Helvetica, Arial, sans-serif; font-size: 15px;"
-    );
-  }
-
-  return (
-    <iframe ref={setContentRef} css={iframe}>
-      {mountNode && createPortal(children, mountNode)}
-    </iframe>
-  );
-};
-
 export const Preview = ({ html }: { html: string }) => {
-  const [parsedHtml, setParsedHtml] = useState(unescapeHtml(html));
-  useEffect(() => {
-    setParsedHtml(unescapeHtml(html));
-  }, [html]);
   return (
     <div>
       <Label>Preview</Label>
-      <IFrame>
-        <div dangerouslySetInnerHTML={{ __html: parsedHtml }} />
-      </IFrame>
+      <iframe srcDoc={unescapeHtml(html)} css={iframe} />
     </div>
   );
 };
