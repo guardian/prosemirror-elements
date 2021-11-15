2105c2bedf7e7874e048f514b65ced44bef27ba4
Rhys Mills <rhystmills@gmail.com>
2021-11-15 10:11:45 +0000

    Add alt text tooltip

--- src/elements/image/ImageElementForm.tsx
+++ src/elements/image/ImageElementForm.tsx
@@ -1,9 +1,10 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { space } from "@guardian/src-foundations";
-import { SvgCamera } from "@guardian/src-icons";
+import { brand, neutral, space } from "@guardian/src-foundations";
+import { SvgCamera, SvgInfo } from "@guardian/src-icons";
 import { Column, Columns } from "@guardian/src-layout";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
+import { usePopper } from "react-popper";
 import { Button } from "../../editorial-source-components/Button";
 import { Error } from "../../editorial-source-components/Error";
 import { FieldWrapper } from "../../editorial-source-components/FieldWrapper";
@@ -44,6 +45,82 @@ const AltText = styled.span`
   margin-right: ${space[2]}px;
 `;
 
+const infoIcon = css`
+  height: 22px;
+  width: 22px;
+  margin-right: 4px;
+  margin-left: -4px;
+  svg {
+    margin: -1px;
+  }
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const tooltip = css`
+  font-family: "Guardian Agate Sans";
+  background-color: ${neutral[10]};
+  color: ${neutral[97]};
+  width: 300px;
+  font-weight: 300;
+  border-radius: 4px;
+  line-height: 1.2rem;
+  font-family: "Guardian Agate Sans";
+  filter: drop-shadow(0 2px 4px rgb(0 0 0 / 30%));
+  z-index: 1;
+  p {
+    margin: 10px;
+  }
+  a {
+    color: ${brand[800]};
+  }
+  opacity: 0;
+  transition: opacity 0.3s;
+  /* transition: visibility 0.3s; */
+  transition: visibility 0s;
+  transition-delay: visibility 0s;
+
+  visibility: hidden;
+  &[data-show] {
+    transition-delay: visibility 1s;
+    transition-delay: transition-delay 1s;
+    visibility: visible;
+    opacity: 1;
+  }
+  [data-popper-reference-hidden] {
+    visibility: hidden;
+    pointer-events: none;
+  }
+`;
+
+const arrow = css`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  background: inherit;
+  visibility: hidden;
+  &[data-show] {
+    ::before {
+      visibility: visible;
+    }
+  }
+  &[data-popper-reference-hidden] {
+    ::before {
+      visibility: hidden;
+      pointer-events: none;
+    }
+  }
+  ::before {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    background: inherit;
+    content: "";
+    transform: translate(0px, -4px) rotate(45deg);
+  }
+`;
+
 export const ImageElementTestId = "ImageElement";
 
 const htmlLength = (text: string) => {
@@ -56,93 +133,168 @@ export const ImageElementForm: React.FunctionComponent<Props> = ({
   errors,
   fields,
   fieldValues,
-}) => (
-  <div data-cy={ImageElementTestId}>
-    <Columns>
-      <Column width={2 / 5}>
-        <FieldLayoutVertical>
-          <CustomDropdownView
-            field={fields.role}
-            label="Weighting"
-            errors={errors.role}
-            options={
-              minAssetValidation(fieldValues.mainImage, "").length
-                ? [thumbnailOption]
-                : undefined
-            }
-          />
-          <ImageView
-            field={fields.mainImage}
-            updateFields={({ caption, source, photographer }) => {
-              fields.caption.update(caption);
-              fields.source.update(source);
-              fields.photographer.update(photographer);
-            }}
-            updateRole={(value) => fields.role.update(value)}
-            errors={errors.mainImage}
-          />
-          <CustomDropdownView
-            field={fields.imageType}
-            label={"Image type"}
-            errors={errors.imageType}
-          />
-        </FieldLayoutVertical>
-      </Column>
-      <Column width={3 / 5}>
-        <FieldLayoutVertical>
-          <FieldWrapper
-            field={fields.caption}
-            errors={errors.caption}
-            headingLabel="Caption"
-            description={`${htmlLength(fieldValues.caption)}/600 characters`}
-          />
-          <FieldWrapper
-            field={fields.alt}
-            errors={errors.alt}
-            headingLabel={<AltText>Alt text</AltText>}
-            headingContent={
-              <Button
-                priority="secondary"
-                size="xsmall"
-                iconSide="left"
-                onClick={() => fields.alt.update(fieldValues.caption)}
-              >
-                Copy from caption
-              </Button>
-            }
-          />
-          <Columns>
-            <Column width={1 / 2}>
-              <FieldWrapper
-                field={fields.photographer}
-                errors={errors.photographer}
-                headingLabel="Photographer"
-              />
-            </Column>
-            <Column width={1 / 2}>
-              <FieldWrapper
-                field={fields.source}
-                errors={errors.source}
-                headingLabel="Source"
-              />
-            </Column>
-          </Columns>
-          <Columns>
-            <Column width={1 / 2}>
-              <CustomCheckboxView
-                field={fields.displayCredit}
-                errors={errors.displayCredit}
-                label="Display credit information"
-              />
-            </Column>
-          </Columns>
-        </FieldLayoutVertical>
-      </Column>
-    </Columns>
-  </div>
-);
+}) => {
+  const [
+    referenceElement,
+    setReferenceElement,
+  ] = useState<HTMLDivElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+    null
+  );
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes, update } = usePopper(
+    referenceElement,
+    popperElement,
+    {
+      placement: "top",
+      modifiers: [
+        { name: "arrow", options: { element: arrowElement } },
+        {
+          name: "offset",
+          options: {
+            offset: [0, 4],
+          },
+        },
+      ],
+    }
+  );
+  return (
+    <div data-cy={ImageElementTestId}>
+      <Columns>
+        <Column width={2 / 5}>
+          <FieldLayoutVertical>
+            <CustomDropdownView
+              field={fields.role}
+              label="Weighting"
+              errors={errors.role}
+              options={
+                minAssetValidation(fieldValues.mainImage, "").length
+                  ? [thumbnailOption]
+                  : undefined
+              }
+            />
+            <ImageView
+              field={fields.mainImage}
+              updateFields={({ caption, source, photographer }) => {
+                fields.caption.update(caption);
+                fields.source.update(source);
+                fields.photographer.update(photographer);
+              }}
+              updateRole={(value) => fields.role.update(value)}
+              errors={errors.mainImage}
+            />
+            <CustomDropdownView
+              field={fields.imageType}
+              label={"Image type"}
+              errors={errors.imageType}
+            />
+          </FieldLayoutVertical>
+        </Column>
+        <Column width={3 / 5}>
+          <FieldLayoutVertical>
+            <FieldWrapper
+              field={fields.caption}
+              errors={errors.caption}
+              headingLabel="Caption"
+              description={`${htmlLength(fieldValues.caption)}/600 characters`}
+            />
+            <FieldWrapper
+              field={fields.alt}
+              errors={errors.alt}
+              headingLabel={<AltText>Alt text</AltText>}
+              headingContent={
+                <>
+                  <div
+                    css={infoIcon}
+                    ref={setReferenceElement}
+                    onMouseOver={() => {
+                      setTooltipVisible(true);
+                      if (update) void update();
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipVisible(false);
+                      if (update) void update();
+                    }}
+                  >
+                    <SvgInfo />
+                  </div>
 
-const imageViewStysles = css`
+                  <div
+                    ref={setPopperElement}
+                    style={styles.popper}
+                    css={tooltip}
+                    data-show={tooltipVisible ? tooltipVisible : null}
+                    {...attributes.popper}
+                    onMouseOver={() => {
+                      setTooltipVisible(true);
+                      if (update) void update();
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipVisible(false);
+                      if (update) void update();
+                    }}
+                  >
+                    <p>
+                      'Alt text' describes what's in an image. It helps users of
+                      screen readers understand our images, and improves our
+                      SEO.
+                    </p>
+                    <p>
+                      <a href="https://example.com">Find out more</a>
+                    </p>
+
+                    <div
+                      ref={setArrowElement}
+                      style={styles.arrow}
+                      css={arrow}
+                      data-show={tooltipVisible ? tooltipVisible : null}
+                    />
+                  </div>
+                  <Button
+                    priority="secondary"
+                    size="xsmall"
+                    iconSide="left"
+                    onClick={() => fields.alt.update(fieldValues.caption)}
+                  >
+                    Copy from caption
+                  </Button>
+                </>
+              }
+            />
+            <Columns>
+              <Column width={1 / 2}>
+                <FieldWrapper
+                  field={fields.photographer}
+                  errors={errors.photographer}
+                  headingLabel="Photographer"
+                />
+              </Column>
+              <Column width={1 / 2}>
+                <FieldWrapper
+                  field={fields.source}
+                  errors={errors.source}
+                  headingLabel="Source"
+                />
+              </Column>
+            </Columns>
+            <Columns>
+              <Column width={1 / 2}>
+                <CustomCheckboxView
+                  field={fields.displayCredit}
+                  errors={errors.displayCredit}
+                  label="Display credit information"
+                />
+              </Column>
+            </Columns>
+          </FieldLayoutVertical>
+        </Column>
+      </Columns>
+    </div>
+  );
+};
+
+const imageViewStyles = css`
   width: 100%;
 `;
 
@@ -200,7 +352,7 @@ const ImageView = ({
     <div>
       <Errors errors={errors.map((e) => e.error)} />
       <div>
-        <img css={imageViewStysles} src={imageSrc} />
+        <img css={imageViewStyles} src={imageSrc} />
       </div>
       <Button
         priority="secondary"
