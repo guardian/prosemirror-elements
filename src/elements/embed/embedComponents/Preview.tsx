31bb815ab693c968b2207f0f37ec8411b9693f61
Rhys Mills <34686302+rhystmills@users.noreply.github.com>
2021-12-13 17:05:30 +0000

    feat: Resize embed preview on content height change, allow manual resize over 100px (#165)
    * Resize preview on content height change, allow manual resize > 100px height
    
    * Memoise the preview element

--- src/elements/embed/embedComponents/Preview.tsx
+++ src/elements/embed/embedComponents/Preview.tsx
@@ -1,17 +1,43 @@
 import { css } from "@emotion/react";
+import React, { useRef, useState } from "react";
 import { Label } from "../../../editorial-source-components/Label";
 import { unescapeHtml } from "./embedUtils";
 
-const iframe = css`
-  background-color: white;
-  width: 100%;
-`;
+export const Preview = React.memo(({ html }: { html: string }) => {
+  const [height, setHeight] = useState("0px");
+  const [resize, setResize] = useState(false);
+  const ref = useRef<HTMLIFrameElement>(null);
+  const maxHeight = 100; // In px
+  const onLoad = () => {
+    const heightOfContent =
+      ref.current?.contentWindow?.document.body.scrollHeight;
+    if (heightOfContent && heightOfContent < maxHeight) {
+      setHeight((heightOfContent + 20).toString() + "px");
+    } else if (heightOfContent && heightOfContent > maxHeight) {
+      setHeight((maxHeight + 20).toString() + "px");
+      setResize(true);
+    } else setResize(false);
+  };
 
-export const Preview = ({ html }: { html: string }) => {
+  const iframe = css`
+    resize: ${resize ? "vertical" : "none"};
+    background-color: white;
+    width: 100%;
+    font-family: sans-serif;
+  `;
+
   return (
     <div>
       <Label>Preview</Label>
-      <iframe srcDoc={unescapeHtml(html)} css={iframe} />
+      <iframe
+        srcDoc={unescapeHtml(html)}
+        css={iframe}
+        ref={ref}
+        onLoad={onLoad}
+        id="myFrame"
+        src="http://demo_iframe.htm"
+        height={height}
+      />
     </div>
   );
-};
+});
