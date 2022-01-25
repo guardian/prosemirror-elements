b9d32d456f100717a514830de57b041024c7f885
Rhys Mills <rhystmills@gmail.com>
2022-01-25 15:22:33 +0000

    Remove values that prevent reduction of size of preview

--- src/elements/helpers/Preview.tsx
+++ src/elements/helpers/Preview.tsx
@@ -17,8 +17,6 @@ const getDocHeight = (doc: Document | undefined) => {
     const height = Math.max(
       body.scrollHeight,
       body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
       html.offsetHeight
     );
     return height;
