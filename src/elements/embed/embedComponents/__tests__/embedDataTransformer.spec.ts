55379ec22f3125fd540631ae1db13fbb085ab6aa
Rhys Mills <34686302+rhystmills@users.noreply.github.com>
2022-02-08 16:54:04 +0000

    fix: Use correct name for 'safeEmbedCode' field (#195)
    * Correct name of 'safeEmbedCode' field
    
    * Update embed tests to refer to safeEmbedCode property

--- src/elements/embed/embedComponents/__tests__/embedDataTransformer.spec.ts
+++ src/elements/embed/embedComponents/__tests__/embedDataTransformer.spec.ts
@@ -37,7 +37,7 @@ const externalElement = (data: Partial<ExternalEmbedFields> = {}) => {
     fields: {
       html: "",
       isMandatory: "false",
-      isSafe: "false",
+      safeEmbedCode: "false",
       role: undefined,
       ...data,
     },
