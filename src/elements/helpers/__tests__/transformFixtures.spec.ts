6332ff4239f6eff01459c45b60cfedbed1e284ab
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-03-03 14:02:34 +0000

    chore: Add fixture based tests, and fixtures for rich-link element (#213)
    * Add fixture based tests, and fixtures for rich-link element
    
    * Add a few comments to explain how to add to fixture tests; update fixtures to avoid JSON

---
+++ src/elements/helpers/__tests__/transformFixtures.spec.ts
@@ -0,0 +1,88 @@
+import { flow, omit } from "lodash";
+import { buildElementPlugin, richlinkElement } from "../../..";
+import { createTestSchema } from "../test";
+import { transformElementIn, transformElementOut } from "../transform";
+import { allElementFixtures } from "./fixtures";
+
+describe("Element fixtures", () => {
+  /**
+   * This spec runs every fixture declared in `./fixtures/index.ts` through
+   * prosemirror-elements' node representation, and out again. It's useful for
+   * spotting any discrepencies that we might inadvertently introduce as we use
+   * prosemirror-elements for more elements in our CMS.
+   *
+   * To add a new fixture, add the fixture JSON to `./fixtures/index.ts`, and
+   * add the relevant element to the `elements` object below, to ensure the
+   * prosemirror-elements plugin recognises the new element type.
+   */
+  const elements = {
+    "rich-link": richlinkElement,
+  } as const;
+
+  const {
+    getElementDataFromNode,
+    getNodeFromElementData,
+    nodeSpec,
+  } = buildElementPlugin(elements);
+
+  const { schema, serializer } = createTestSchema(nodeSpec);
+
+  /**
+   * Creates a function that passes element data through our data
+   * transformers, into a Prosemirror node, and then back out through the
+   * transformers again.
+   *
+   * This reflects what happens when an element is parsed into a document in
+   * Composer, and then parsed back out to be saved.
+   */
+  const createRoundTripElementData = (elementName: keyof typeof elements) =>
+    flow(
+      (flexElement) => transformElementIn(elementName, flexElement),
+      (values) => getNodeFromElementData({ elementName, values }, schema),
+      (node) => node && getElementDataFromNode(node, serializer),
+      (maybePMEElement) =>
+        maybePMEElement &&
+        transformElementOut(maybePMEElement.elementName, maybePMEElement.values)
+    );
+
+  // Run fixtures for each element specified in `./fixtures/index.ts` through our round-trip tests.
+  Object.entries(allElementFixtures).forEach(
+    ([elementName, elementFixtures]) => {
+      describe(`round-tripping data for the ${elementName} element fixtures in and out of prosemirror-elements' node representation`, () => {
+        it("should not change element data, unless we are knowingly supplying new defaults", () => {
+          const roundTripElementData = createRoundTripElementData(
+            elementName as keyof typeof elements
+          );
+          elementFixtures.forEach(({ element }) => {
+            const roundTrippedElementData = roundTripElementData(element);
+
+            /**
+             * We expect certain differences in our elements, as both role and isMandatory
+             * properties may historically be absent but will now be added as default.
+             * If our element data does not have these properties, we ignore them in the new
+             * element data.
+             */
+            const ignoredFieldValues = [];
+            if (!element.fields.isMandatory) {
+              ignoredFieldValues.push("fields.isMandatory");
+            }
+            if (!element.fields.role) {
+              ignoredFieldValues.push("fields.role");
+            }
+            const elementToCompare = omit(
+              element,
+              "elementType",
+              ...ignoredFieldValues
+            );
+            const roundTrippedElementToCompare = omit(
+              roundTrippedElementData,
+              ...ignoredFieldValues
+            );
+
+            expect(roundTrippedElementToCompare).toEqual(elementToCompare);
+          });
+        });
+      });
+    }
+  );
+});
