ad9de02d3e7cd4d34c928766c13910ecfe77ac33
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-05-16 17:38:08 +0100

    Speed up tests with --runInBand

--- src/declarations/fixtures.d.ts
+++ src/declarations/fixtures.d.ts
@@ -2,8 +2,10 @@
 // We declare these types explicitly to avoid Typescript inferring types.
 declare module "*.fixture.json" {
   const content: Array<{
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- our element is used for fixture tests, so the type is arbitrary
-    element: any;
+    element: {
+      assets: unknown[];
+      fields: Record<string, unknown>;
+    };
     created: string;
     contentid: string;
   }>;
