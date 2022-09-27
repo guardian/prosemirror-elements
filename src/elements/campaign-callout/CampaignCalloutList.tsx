5558fd69afe92339c4123e4c1ef9f0ae85924912
Anna Beddow <anna.beddow@gmail.com>
2022-09-27 11:23:07 +0100

    Temp: add change handler to callout dropdown to render callout element

--- src/elements/campaign-callout/CampaignCalloutList.tsx
+++ src/elements/campaign-callout/CampaignCalloutList.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { FieldNameToField } from "../../plugin/types/Element";
 import { CustomDropdownView } from "../../renderers/react/customFieldViewComponents/CustomDropdownView";
-import { createCampaignCalloutListFields } from "./CampaignCalloutListSpec";
+import { Callout } from "../embed/Callout";
+import {
+  Campaign,
+  createCampaignCalloutListFields,
+} from "./CampaignCalloutListSpec";
 type Props = {
   fields: FieldNameToField<ReturnType<typeof createCampaignCalloutListFields>>;
+  campaigns: Campaign[];
 };
 
 export const CampaignCalloutList: React.FunctionComponent<Props> = ({
   fields,
+  campaigns,
 }) => {
-  return (
+  const view = fields.campaignList.view;
+  // how do we access the view.node.attrs.field to get the stored id value
+  const [calloutId, setCalloutId] = useState<string>();
+
+  const onChange = (calloutId: string) => {
+    setCalloutId(calloutId);
+  };
+
+  const getTag = (id: string) => {
+    const campaign = campaigns.find((campaign) => campaign.id === id);
+    return campaign?.fields.tagName || "";
+  };
+
+  return calloutId ? (
+    <Callout
+      tag={getTag(calloutId)}
+      targetingUrl={"https://targeting.code.dev-gutools.co.uk/"}
+    />
+  ) : (
     <div>
-      <CustomDropdownView label="Callout" field={fields.campaignList} />
+      <CustomDropdownView
+        label="Callout"
+        field={fields.campaignList}
+        changeHandler={onChange}
+      />
     </div>
   );
 };
