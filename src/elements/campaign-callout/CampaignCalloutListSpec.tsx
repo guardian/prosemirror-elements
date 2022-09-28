89edf3ff0cadf4a0c200dd6fc09a717c963edff8
Anna Beddow <anna.beddow@gmail.com>
2022-09-28 11:07:54 +0100

    Feat: temp make fields optional

--- src/elements/campaign-callout/CampaignCalloutListSpec.tsx
+++ src/elements/campaign-callout/CampaignCalloutListSpec.tsx
@@ -7,27 +7,27 @@ import { Callout } from "../embed/Callout";
 import { undefinedDropdownValue } from "../helpers/transform";
 
 type Fields = {
-  callout: string;
-  formId: number;
-  tagName: string;
-  description: string;
-  formUrl: string;
-  _type: string;
+  callout?: string;
+  formId?: number;
+  tagName?: string;
+  description?: string;
+  formUrl?: string;
+  _type?: string;
 };
 
 type Rules = {
-  requiredTags: string[];
-  lackingTags: string[];
-  matchAllTags: boolean;
+  requiredTags?: string[];
+  lackingTags?: string[];
+  matchAllTags?: boolean;
 };
 export type Campaign = {
   id: string;
-  fields: Fields;
-  rules: Rules[];
-  priority: number;
-  displayOnSensitive: boolean;
-  activeFrom: number;
-  activeUntil: number;
+  fields?: Fields;
+  rules?: Rules[];
+  priority?: number;
+  displayOnSensitive?: boolean;
+  activeFrom?: number;
+  activeUntil?: number;
   name: string;
 };
 type Props = {
