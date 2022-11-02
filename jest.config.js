6b48abf51b01d56de599a2ad3dc3f0cc55b352a5
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-11-02 10:10:22 +0000

    Remove setup file from jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   roots: ["<rootDir>/src"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testMatch: ["**/?(*.)(spec).ts?(x)"]
 };
