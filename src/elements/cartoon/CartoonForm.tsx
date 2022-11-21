9a3f7ec7f6d5303fdeadce16844332f9d45c64e5
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2022-11-21 16:32:25 +0000

    add ability to remove images

--- src/elements/cartoon/CartoonForm.tsx
+++ src/elements/cartoon/CartoonForm.tsx
@@ -1,7 +1,9 @@
 import { css } from "@emotion/react";
+import { neutral } from "@guardian/src-foundations";
+import { iconSize } from "@guardian/src-foundations/size";
 import { Column, Columns } from "@guardian/src-layout";
 import type { FunctionComponent } from "react";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Button } from "../../editorial-source-components/Button";
 import { FieldWrapper } from "../../editorial-source-components/FieldWrapper";
 import { Label } from "../../editorial-source-components/Label";
@@ -22,58 +24,53 @@ export const cartoonElement = createReactElementSpec(
       fields.mobileImages.value
     );
 
+    useEffect(() => {
+      fields.desktopImages.update(desktopImages);
+    }, [desktopImages]);
+
+    useEffect(() => {
+      fields.mobileImages.update(mobileImages);
+    }, [mobileImages]);
+
     return (
       <>
         <Label>Cartoon</Label>
         <Columns>
           <Column width={1 / 3}>
             <FieldLayoutVertical>
-              <Label>Desktop View</Label>
-              {desktopImages.map((image) => (
-                <ImageThumbnail image={image} altText={fields.alt.value} />
-              ))}
-              <Button
-                priority="secondary"
-                size="xsmall"
-                onClick={() => {
+              <ImageSet
+                title={"Desktop view"}
+                images={desktopImages}
+                defaultAltText={fields.alt.value}
+                addImage={() => {
                   fields.desktopImages.description.props.openImageSelector(
-                    (mediaPayload: MediaPayload) => {
-                      fields.desktopImages.update([
-                        ...desktopImages,
-                        mediaPayload,
-                      ]);
-                      setDesktopImages([...desktopImages, mediaPayload]);
-                    }
+                    (mediaPayload: MediaPayload) =>
+                      setDesktopImages([...desktopImages, mediaPayload])
                   );
                 }}
-              >
-                Add image
-              </Button>
+                removeImage={(index) => {
+                  setDesktopImages(desktopImages.filter((_, i) => i !== index));
+                }}
+                required={true}
+              />
             </FieldLayoutVertical>
           </Column>
           <Column width={1 / 3}>
             <FieldLayoutVertical>
-              <Label>Mobile View</Label>
-              {mobileImages.map((image) => (
-                <ImageThumbnail image={image} altText={fields.alt.value} />
-              ))}
-              <Button
-                priority="secondary"
-                size="xsmall"
-                onClick={() => {
-                  fields.desktopImages.description.props.openImageSelector(
-                    (mediaPayload: MediaPayload) => {
-                      fields.mobileImages.update([
-                        ...mobileImages,
-                        mediaPayload,
-                      ]);
-                      setMobileImages([...mobileImages, mediaPayload]);
-                    }
+              <ImageSet
+                title={"Mobile view"}
+                images={mobileImages}
+                defaultAltText={fields.alt.value}
+                addImage={() => {
+                  fields.mobileImages.description.props.openImageSelector(
+                    (mediaPayload: MediaPayload) =>
+                      setMobileImages([...mobileImages, mediaPayload])
                   );
                 }}
-              >
-                Add image
-              </Button>
+                removeImage={(index) => {
+                  setMobileImages(mobileImages.filter((_, i) => i !== index));
+                }}
+              />
             </FieldLayoutVertical>
           </Column>
 
@@ -105,11 +102,60 @@ export const cartoonElement = createReactElementSpec(
   }
 );
 
+const ImageSet: FunctionComponent<{
+  title: string;
+  images: MainImageData[];
+  defaultAltText: string;
+  addImage: () => void;
+  removeImage: (index: number) => void;
+  required?: boolean;
+}> = ({
+  title,
+  images,
+  defaultAltText,
+  addImage,
+  removeImage,
+  required = false,
+}) => {
+  return (
+    <>
+      <Label>{title}</Label>
+      {images.map((image, index) => (
+        <ImageThumbnail
+          index={index}
+          image={image}
+          altText={defaultAltText}
+          removeImage={removeImage}
+          required={required && index === 0}
+        />
+      ))}
+      <Button priority="secondary" size="xsmall" onClick={() => addImage()}>
+        Add image
+      </Button>
+    </>
+  );
+};
+
 const imageThumbnailStyle = css`
   position: relative;
   width: 150px;
   height: 150px;
   background-color: #ccc;
+  button {
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    padding: 0;
+    border: 0;
+    background: none;
+    cursor: pointer;
+    svg {
+      fill: ${neutral[20]};
+    }
+    :hover svg {
+      fill: ${neutral[60]};
+    }
+  }
   img {
     position: absolute;
     max-width: 100%;
@@ -120,11 +166,23 @@ const imageThumbnailStyle = css`
 `;
 
 const ImageThumbnail: FunctionComponent<{
+  index: number;
   image: MainImageData;
   altText: string;
-}> = ({ image, altText }) => {
+  removeImage: (index: number) => void;
+  required: boolean;
+}> = ({ index, image, altText, removeImage, required }) => {
   return (
     <div css={imageThumbnailStyle}>
+      {!required && (
+        <button
+          role="button"
+          aria-label={"Remove Image"}
+          onClick={() => removeImage(index)}
+        >
+          <SvgCrossRound />
+        </button>
+      )}
       <img
         src={useMemo(() => getImageSrc(image.assets, 1200), [image.assets])}
         alt={altText}
@@ -132,3 +190,17 @@ const ImageThumbnail: FunctionComponent<{
     </div>
   );
 };
+
+const SvgCrossRound: FunctionComponent = () => (
+  <svg
+    width={iconSize["small"]}
+    viewBox="-3 -3 30 30"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm5.138-14.358-.782-.78-4.349 3.982-4.364-3.967-.782.78L10.85 12l-3.988 4.342.782.781 4.364-3.967 4.35 3.982.781-.78L13.165 12l3.973-4.358Z"
+    />
+  </svg>
+);
