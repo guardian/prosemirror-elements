2c6a2a9c1dae4b5dc312c79f4859f854c089cc98
Anna Beddow <anna.beddow@gmail.com>
2022-11-29 14:38:12 +0000

    Feat: Add a callout error component

---
+++ src/elements/callout/CalloutError.tsx
@@ -0,0 +1,67 @@
+import { css } from "@emotion/react";
+import { space, text } from "@guardian/src-foundations";
+import { SvgAlertTriangle } from "@guardian/src-icons";
+import { Error } from "../../editorial-source-components/Error";
+import { Campaign } from "./Callout";
+import { CalloutTableHeader } from "./CalloutTable";
+
+const marginBottom = css`
+  margin-bottom: ${space[2]}px !important;
+`;
+
+const error = css`
+  display: flex;
+  align-items: center;
+  svg {
+    height: 20px;
+    fill: ${text.error};
+    margin-right: ${space[1]}px;
+  }
+`;
+export const CalloutError = ({
+  callout,
+  targetingUrl,
+  calloutId,
+  isActiveCallout,
+}: {
+  callout: Campaign | undefined;
+  targetingUrl: string;
+  calloutId: string;
+  isActiveCallout: boolean;
+}) => {
+  const edToolsEmail = "editorial.tools.dev@theguardian.com";
+  const centralProdEmail = "central.production@theguardian.com";
+
+  return callout && !isActiveCallout ? (
+    <>
+      <Error css={[error, marginBottom]}>
+        <SvgAlertTriangle />
+        <p>This callout has expired and will not appear in the article.</p>
+      </Error>
+      <CalloutTableHeader
+        title={callout.fields.callout}
+        tagName={callout.fields.tagName}
+        targetingUrl={targetingUrl}
+        calloutId={calloutId}
+        formUrl={callout.fields.formUrl ?? ""}
+      />
+    </>
+  ) : (
+    <div>
+      <Error css={[error, marginBottom]}>
+        <SvgAlertTriangle />
+        Composer was unable to find this callout.
+      </Error>
+      <p css={marginBottom}>
+        It is likely that the callout has been deleted. Please check in the
+        targeting tool to check if this callout is available.
+      </p>
+      <p>
+        If the problem persists, you may wish to contact Central Production (
+        <a href={`mailto:${centralProdEmail}`}>{centralProdEmail}</a>) and the
+        Editorial Tools team (
+        <a href={`mailto:${edToolsEmail}`}>{edToolsEmail}</a>) for assistance.
+      </p>
+    </div>
+  );
+};
