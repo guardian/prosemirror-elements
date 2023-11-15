89ed4ab091db3801dac4fe173262f0ae972c3755
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-11-15 10:46:21 +0000

    First pass at recipe structure and form

--- src/elements/recipe/RecipeElementForm.tsx
+++ src/elements/recipe/RecipeElementForm.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { FieldLayoutVertical } from "../../editorial-source-components/FieldLayout";
 import { FieldWrapper } from "../../editorial-source-components/FieldWrapper";
 import { createReactElementSpec } from "../../renderers/react/createReactElementSpec";
 import { recipeFields } from "./RecipeElementSpec";
+import styled from "@emotion/styled";
 
 export const RecipeElementTestId = "RecipeElement";
 
+const RecipeSection: React.FC<{
+  title: string;
+  isOpen: boolean;
+  toggleOpenState: () => void;
+}> = ({ title, children, isOpen, toggleOpenState }) => (
+  <>
+    <h2 onClick={() => toggleOpenState()}>
+      {title}
+      {isOpen ? "▼" : "▶"}
+    </h2>
+    {isOpen && children}
+  </>
+);
+
+const initialOpenStates = {
+  ingredients: false,
+  instructions: false,
+};
+
 export const recipeElement = createReactElementSpec(
   recipeFields,
-  ({ fields }) => (
-    <FieldLayoutVertical data-cy={RecipeElementTestId}>
-      <FieldWrapper headingLabel="Recipe" field={fields.recipeJson} />
-    </FieldLayoutVertical>
-  )
+  ({ fields }) => {
+    const [openStates, setOpenStates] = useState(initialOpenStates);
+
+    const toggleOpenState = React.useMemo(
+      () => (groupName: keyof typeof openStates) => () =>
+        setOpenStates({ ...openStates, [groupName]: !openStates[groupName] }),
+      [openStates]
+    );
+
+    return (
+      <FieldLayoutVertical data-cy={RecipeElementTestId}>
+        <FieldWrapper headingLabel="Title" field={fields.title} />
+        <FieldWrapper headingLabel="Description" field={fields.title} />
+        <RecipeSection
+          isOpen={openStates.ingredients}
+          toggleOpenState={toggleOpenState("ingredients")}
+          title="Ingredients"
+        ></RecipeSection>
+        <RecipeSection
+          isOpen={openStates.instructions}
+          toggleOpenState={toggleOpenState("instructions")}
+          title="Instructions"
+        ></RecipeSection>
+      </FieldLayoutVertical>
+    );
+  }
 );
--- src/elements/recipe/RecipeElementSpec.tsx
+++ src/elements/recipe/RecipeElementSpec.tsx
@@ -1,12 +1,61 @@
+import { createCustomField } from "../../plugin/fieldViews/CustomFieldView";
+import { createRepeaterField } from "../../plugin/fieldViews/RepeaterFieldView";
+import { createRichTextField } from "../../plugin/fieldViews/RichTextFieldView";
 import { createTextField } from "../../plugin/fieldViews/TextFieldView";
-import { required } from "../../plugin/helpers/validation";
+import type { MediaPayload } from "../helpers/types/Media";
+
+type Tag = {};
+type DifficultyLevel = "easy" | "medium" | "hard";
+type TimingQualifier = "passive" | "active" | "set" | "chill";
+type Serves = {};
+
+const ingredient = {
+  name: createTextField(), // no text editor
+  ingredientID: createTextField({ absentOnEmpty: true }), // no text editor
+  amount: createCustomField<Range | undefined>(undefined, undefined),
+  unit: createTextField({ absentOnEmpty: true }),
+  prefix: createTextField({ absentOnEmpty: true }),
+  suffix: createTextField({ absentOnEmpty: true }),
+  text: createTextField({ absentOnEmpty: true }), // no text editor
+  optional: createCustomField<boolean>(false, undefined),
+};
+
+const ingredientsGroup = {
+  recipeSection: createTextField(),
+  ingredientsList: createRepeaterField(ingredient),
+};
+
+const timingFields = {
+  qualifier: createCustomField<TimingQualifier>("passive", undefined),
+  duration: createCustomField<number>(0, undefined),
+  text: createTextField(), // no text editor,
+};
+
+const instructionFields = {
+  stepNumber: createCustomField<number>(1, undefined),
+  description: createRichTextField({
+    placeholder: "Describe this step of the recipe …",
+  }),
+  images: createCustomField<MediaPayload[]>([], undefined),
+};
 
 export const recipeFields = {
-  recipeJson: createTextField({
-    rows: 4,
-    isResizeable: true,
-    isCode: true,
-    validators: [required("empty recipe field")],
-    absentOnEmpty: true,
-  }),
+  id: createTextField(), // no text editor
+  canonicalArticle: createTextField(), // no text editor
+  title: createTextField(),
+  description: createTextField(),
+  featureImage: createCustomField<MediaPayload | undefined>(undefined, {}),
+  contributors: createCustomField<Tag[]>([], undefined),
+  byline: createCustomField<string>("", {}),
+  ingredients: createRepeaterField(ingredientsGroup),
+  suitableForDietIds: createCustomField<string[]>([], undefined),
+  cuisineIds: createCustomField<string[]>([], undefined),
+  mealTypeIds: createCustomField<string[]>([], undefined),
+  celebrationIds: createCustomField<string[]>([], undefined),
+  utilsAndApplianceIds: createCustomField<string[]>([], undefined),
+  techniquesUsedIds: createCustomField<string[]>([], undefined),
+  difficultyLevel: createCustomField<DifficultyLevel>("easy", undefined),
+  serves: createCustomField<Serves | undefined>(undefined, undefined),
+  timings: createRepeaterField(timingFields),
+  instructions: createRepeaterField(instructionFields),
 };
