d4808865a38737df88eaed687aa73e044c7aa6cf
Rhys Mills <34686302+rhystmills@users.noreply.github.com>
2024-05-31 10:04:45 +0100

    Fix recently introduce bug affecting elements (#406)
    * Make tests more specific regarding whether selection or content have changed
    
    * Make sure diffs that start at pos 0 are valid for content changes
    
    * Add tests covering the case that had a bug
    
    * Also check end pos is not null
    
    * Add changeset for bug fiz

---
+++ .changeset/chilly-eyes-kick.md
@@ -0,0 +1,5 @@
+---
+"@guardian/prosemirror-elements": patch
+---
+
+Fix bug for elements changes that start at pos 0
--- src/plugin/fieldViews/ProseMirrorFieldView.ts
+++ src/plugin/fieldViews/ProseMirrorFieldView.ts
@@ -197,7 +197,8 @@ export abstract class ProseMirrorFieldView extends FieldView<string> {
     const diffStart = node.content.findDiffStart(state.doc.content);
     const diffEnd = node.content.findDiffEnd(state.doc.content);
 
-    if (diffStart && diffEnd) {
+    // Check for null specifically, rather than falsiness, because a diff starting at pos 0 is a valid diff
+    if (diffStart !== null && diffEnd !== null) {
       let { a: endOfOuterDiff, b: endOfInnerDiff } = diffEnd;
       // This overlap accounts for a situation where we're diffing nodes where we encounter
       // identical content.
