3fd5b1c049744148577dcaea0806fe7af92ad2ea
Rhys Mills <rhys.mills@guardian.co.uk>
2024-07-01 17:01:46 +0100

    Update the outer editor when the inner editor's menu has updated the stored marks

--- src/plugin/fieldViews/ProseMirrorFieldView.ts
+++ src/plugin/fieldViews/ProseMirrorFieldView.ts
@@ -302,7 +302,16 @@ export abstract class ProseMirrorFieldView extends FieldView<string> {
       outerTr.setMeta("paste", true);
     }
 
-    const shouldUpdateOuter = innerTr.docChanged || selectionHasChanged;
+    const storedMarksHaveChanged = !isEqual(
+      this.outerView.state.storedMarks,
+      innerTr.storedMarks
+    );
+
+    if (storedMarksHaveChanged){
+      outerTr.setStoredMarks(innerTr.storedMarks);
+    }
+    
+    const shouldUpdateOuter = innerTr.docChanged || selectionHasChanged || storedMarksHaveChanged;
     if (shouldUpdateOuter) this.dispatchToOuterView(outerTr);
   }
 
