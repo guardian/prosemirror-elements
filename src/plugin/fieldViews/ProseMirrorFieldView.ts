6c5bb676236a6c57ab215b332010c872bf779537
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-08-19 09:37:54 +0100

    Move DecorationGroup declaration to declaration file

--- src/plugin/fieldViews/ProseMirrorFieldView.ts
+++ src/plugin/fieldViews/ProseMirrorFieldView.ts
@@ -4,7 +4,11 @@ import type { AttributeSpec, Mark, Node } from "prosemirror-model";
 import type { Plugin, Selection, Transaction } from "prosemirror-state";
 import { EditorState } from "prosemirror-state";
 import { Mapping, StepMap } from "prosemirror-transform";
-import type { DecorationSource, EditorProps } from "prosemirror-view";
+import type {
+  DecorationGroup,
+  DecorationSource,
+  EditorProps,
+} from "prosemirror-view";
 import { DecorationSet, EditorView } from "prosemirror-view";
 import type { PlaceholderOption } from "../helpers/placeholder";
 import { PME_UPDATE_PLACEHOLDER } from "../helpers/placeholder";
@@ -21,18 +25,6 @@ export interface AbstractTextFieldDescription
   absentOnEmpty?: boolean;
   // Can the user resize the input?
   isResizeable?: boolean;
-}
-
-// The type of this class isn't made available by prosemirror-view - we should request a change there to make
-// it available as we have to interact with instance of it
-declare class DecorationGroup implements DecorationSource {
-  readonly members: readonly DecorationSet[];
-  constructor(members: readonly DecorationSet[]);
-  map(mapping: Mapping, doc: Node): DecorationSource;
-  forChild(offset: number, child: Node): DecorationSource | DecorationSet;
-  eq(other: DecorationGroup): boolean;
-  locals(node: Node): readonly any[];
-  static from(members: readonly DecorationSource[]): DecorationSource;
 }
 
 /**
