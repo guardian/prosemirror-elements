bf7df09c205d1949ea32a6c71e7b27e3490fe824
Rhys Mills <34686302+rhystmills@users.noreply.github.com>
2024-09-28 12:31:30 +0100

    Remove flaky test, because it frequently causes problems in CI (#434)

--- cypress/tests/ImageElement.cy.ts
+++ cypress/tests/ImageElement.cy.ts
@@ -405,19 +405,6 @@ describe("ImageElement", () => {
         assertDocHtml(getSerialisedHtml({ codeValue: "Code \n text" }));
       });
 
-      it(`should not reach its max height when text has fewer rows than the number of lines specified by maxRows`, () => {
-        addImageElement({ code: "Code \n \n \n \n \n text" });
-        getElementRichTextField("code")
-          .invoke("css", "height")
-          .then((height) =>
-            getElementRichTextField("code")
-              .invoke("css", "max-height")
-              // Chained expressions here allow us to compare numerical value of string px properties
-              .then((maxHeight) => parseInt(maxHeight.toString()))
-              .should("be.above", parseInt(height.toString()))
-          );
-      });
-
       it(`should visually extend no more than the number of lines specified by maxRows`, () => {
         addImageElement({ code: "Code \n \n \n \n \n \n \n \n \n \n \n text" });
         getElementRichTextField("code")
